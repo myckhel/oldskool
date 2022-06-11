0f357b835e83d95adc642841801c202234a07cb2
myckhel <myckhel1@hotmail.com>
2022-06-11 01:32:14 +0100

    created admin model

---
+++ app/Models/Admin.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+
+class Admin extends Model
+{
+    use HasFactory, Notifiable;
+
+    protected $fillable = ['firstname', 'lastname', 'email', 'phone'];
+}
